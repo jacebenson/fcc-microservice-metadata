270a0f1c194fb24836f1207e78b3ba039cdf47e4
Jace Benson <jace.benson@gmail.com>
2017-01-22 22:50:46 -0600

    done!

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 
-var multer  = require('multer');
+var multer = require('multer');
 var storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
 
@@ -12,20 +12,21 @@ var upload = multer({ storage: storage });
  */
 app.use(express.static('views'));
 app.get('/', function (request, response) {
-    response.sendFile('index.html');
+  response.sendFile('index.html');
 });
 
 app.post('/', upload.single('uploadedFile'), function (request, response) {
-    if (request.file) {
-        response.send({
-            size: request.file.size
-        });
+  if (request.file) {
+    response.setHeader('Content-Type: application/json')
+    response.send({
+      size: request.file.size
+    });
 
-    } else {
-        response.send({
-            error: "It was not possible to upload the file, please try again."
-        })
-    }
+  } else {
+    response.send({
+      error: "It was not possible to upload the file, please try again."
+    })
+  }
 });
 
 app.listen(process.env.PORT || 5000);
