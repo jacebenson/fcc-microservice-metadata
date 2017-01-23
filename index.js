270a6df0e912750485d9c2d8ed0ec6c9dccef304
Jace Benson <jace.benson@gmail.com>
2017-01-22 22:46:23 -0600

    ....

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 var express = require('express');
 var app = express();
+
 var multer  = require('multer');
-app.use(express.static('form'));
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+
+app.use(express.static('views'));
+
 app.get('/', function (request, response) {
     response.sendFile('index.html');
 });
+
+app.post('/', upload.single('uploadedFile'), function (request, response) {
+    if(request.file) {
+        response.send({
+            'size': request.file.size
+        });
+
+    } else {
+        response.send({
+            "error": "It was not possible to upload the file, please try again."
+        })
+    }
+});
+
+app.listen(process.env.PORT || 8080);
