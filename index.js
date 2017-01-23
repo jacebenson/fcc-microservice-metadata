28528864f89360f501c816a0a316abafff050c23
Jace Benson <jace.benson@gmail.com>
2017-01-22 22:44:27 -0600

    ...

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var express = require('express');
 var app = express();
 var multer  = require('multer');
-app.use(express.static('/'));
+app.use(express.static('form'));
+app.get('/', function (request, response) {
+    response.sendFile('index.html');
+});
